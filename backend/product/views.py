1315b498e4d652e5d3617b7a7a47a092694bcf6a
Lee2523 <seungoh0423@gmail.com>
2022-02-02 01:40:05 +0900

    feat : product list

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from django.db import connections
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Product
+from .serializers import ProductSerializer
+
 # Create your views here.
 class ProductList(APIView):
     def get(self, request):
-        pass
+        queryset = Product.objects.filter().order_by("-idx")
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
