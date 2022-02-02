63461493f7ce16c6555681571552779707a9205e
Lee2523 <seungoh0423@gmail.com>
2022-02-02 21:52:06 +0900

    feat : vue에서 상품 등록

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+    
   <section>
     <div v-for="product in products" :key="product.idx" class="products">
       <div class="card" style="width: 18rem;">
@@ -14,7 +16,39 @@
       <br/>
     </div>
   </section>
+  <hr/><br/>
+  <section>
+        <h1>상품등록</h1>
+        <hr/><br/>
+
+        <form @submit.prevent="submit">
+          <div class="mb-3">
+            <label for="title" class="form-label">상품명:</label>
+            <input type="text" name="title" v-model="form.title" class="form-control" />
+          </div>
+          <div class="mb-3">
+            <label for="brand" class="form-label">브랜드명:</label>
+            <input type="text" name="brand" v-model="form.brand" class="form-control" />
+          </div>
+          <div class="mb-3">
+            <label for="content" class="form-label">본문:</label>
+            <textarea
+              name="content"
+              v-model="form.content"
+              class="form-control"
+            ></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="price" class="form-label">가격:</label>
+            <input type="text" name="price" v-model="form.price" class="form-control" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </section>
+
+  </div>
 </template>
+
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
